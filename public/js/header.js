29bc60039f0099b3f5d4a89f968006db85032d02
AtsukiTak <takatomgoo@gmail.com>
2019-04-30 02:35:45 +0900

    recruit pages

--- public/js/header.js
+++ public/js/header.js
@@ -1,11 +1,11 @@
-$(function(){
+document.addEventListener('DOMContentLoaded', function(){
   // メニューボタンを押した時の挙動
-  $("header > .mobile.bar > .menu-btn").on("click", function() {
-    $("header > .mobile.contents").show();
-  });
+  document.querySelector("header > .mobile.bar > .menu-btn").onclick = function() {
+    document.querySelector("header > .mobile.contents").style.display = "block";
+  };
 
   // 閉じるボタンを押した時の挙動
-  $("header > .mobile.contents > .close-btn").on("click", function() {
-    $("header > .mobile.contents").hide();
-  })
-});
+  document.querySelector("header > .mobile.contents > .close-btn").onclick = function() {
+    document.querySelector("header > .mobile.contents").style.display = "none";
+  };
+}, false);
